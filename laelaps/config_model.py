9b4f048b2434176085f502834aa48682865609a5
JanMaartenvanDoorn <laelaps@vandoorn.cloud>
2025-12-06 16:37:22 +0100

    reformat

--- laelaps/config_model.py
+++ laelaps/config_model.py
@@ -64,7 +64,4 @@ class ConfigModel(BaseSettings):
     user: UserConfigModel
     encryption: EncryptionConfigModel
 
-    model_config = {
-        "env_prefix": "LAELAPS_",
-        "env_nested_delimiter": "__"
-    }
+    model_config = {"env_prefix": "LAELAPS_", "env_nested_delimiter": "__"}
